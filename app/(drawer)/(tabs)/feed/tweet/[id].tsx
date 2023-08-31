d005addafa08d90d9de3bcf1599d84cba9028af6
Yueming Pang <pymdebaby@gmail.com>
2023-08-31 19:15:45 -0400

    implemented show one tweet

--- app/(drawer)/(tabs)/feed/tweet/[id].tsx
+++ app/(drawer)/(tabs)/feed/tweet/[id].tsx
@@ -1,15 +1,24 @@
-import { Text } from 'react-native';
+import { ActivityIndicator, Text } from 'react-native';
 import Tweet from '../../../../../components/Tweet';
-import tweets from '../../../../../assets/data/tweets';
+//import tweets from '../../../../../assets/data/tweets';
 import { useGlobalSearchParams } from 'expo-router';
+import { useQuery } from '@tanstack/react-query';
+import { getTweet } from '../../../../../lib/api/tweet';
 
 export default function TweetScreen() {
     const {id} = useGlobalSearchParams();
 
-    const tweet = tweets.find(t => t.id == id);
-    if (!tweet){
+    const {data, isLoading, error} = useQuery({
+        queryKey: ['tweet', id],
+        queryFn: () => getTweet(id as string),
+    });
+
+    if (isLoading) {
+        return <ActivityIndicator />
+    }
+    if (error){
         return <Text>Tweet {id} not found!</Text>
     }
 
-    return <Tweet tweet={tweet} />;
+    return <Tweet tweet={data} />;
 }
--- lib/api/tweet.ts
+++ lib/api/tweet.ts
@@ -1,17 +1,31 @@
 import { API_URL, authToken } from "./config";
 
 export const listTweets = async () => {
-    const res = await fetch(`${API_URL}/tweet`, {
-      headers: {
-        Authorization: `Bearer ${authToken}`,
-      }
-    }); //REST API get method
-    if (res.status == 401) {
-      throw new Error("Authorization failed");
+  const res = await fetch(`${API_URL}/tweet`, {
+    headers: {
+      Authorization: `Bearer ${authToken}`,
     }
-    if (res.status != 200) {
-      throw new Error("Error fetching the API");
+  }); //REST API get method
+  if (res.status == 401) {
+    throw new Error("Authorization failed");
+  }
+  if (res.status != 200) {
+    throw new Error("Error fetching the API");
+  }
+  return await res.json();
+};
+
+export const getTweet = async (id: string) => {
+  const res = await fetch(`${API_URL}/tweet/${id}`, {
+    headers: {
+      Authorization: `Bearer ${authToken}`,
     }
-    const data = await res.json();
-    return data;
+  }); //REST API get method
+  if (res.status == 401) {
+    throw new Error("Authorization failed");
+  }
+  if (res.status != 200) {
+    throw new Error("Error fetching the API");
+  }
+  return await res.json();
 }
